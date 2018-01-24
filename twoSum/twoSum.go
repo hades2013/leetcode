a550076a4de5364517f10aeafc0f84bad43be77f
xutiancheng <xutiancheng@hexicomtech.com>
2018-01-24 21:19:09 +0800

    add golang another function

--- twoSum/twoSum.go
+++ twoSum/twoSum.go
@@ -4,6 +4,24 @@ import (
     "fmt"
 )
 
+func twoSumA(nums []int, target int) []int {
+    var m map[int]int
+    m = make(map[int]int)
+    for k, v := range nums {
+        m[v] = k
+    }
+    for k, v := range nums {
+        if _, ok := m[target-v]; ok {
+            if m[target-v] == k {
+                continue
+            } else {
+                return []int{k, m[target-v]}
+            }
+        }
+    }
+    return []int{}
+}
+
 func twoSum(nums []int, target int) []int {
     for i,v1 := range nums{
 	if i+1 != len(nums){
@@ -21,6 +39,6 @@ func main() {
     nums := []int{2, 7, 11, 15}
     fmt.Println(nums[0:])
     target := 9
-    output := twoSum(nums, target)
+    output := twoSumA(nums, target)
     fmt.Println(output)
 }
