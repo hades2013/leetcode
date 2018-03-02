4e145abfd3f30ceb73c1a8d0a3fa8d2f3d0fbc16
xutiancheng <xutiancheng@hexicomtech.com>
2018-03-02 11:24:14 +0800

    modify valid parentheses C

--- validParentheses/validParentheses.c
+++ validParentheses/validParentheses.c
@@ -1,9 +1,59 @@
+/*
+* given a string containing (){}[]. determine if the string is valid
+* athor: hades
+* date: 2018-2 
+* modify date : 2018-3-2
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 
-bool  isValid(char* s) {  
+bool isValidB(char* s) {
+     int len = strlen(s);
+    if( len%2 != 0 )
+    {
+        return false;
+    }
+    
+    int limit = len / 2;
+    char *stack = malloc(limit);
+    int idx = 0;
+    char cur, peek;
+    int i; 
+    for(i = 0; i < len; ++i)
+    {
+        cur = s[i];
+        if(cur == '(' || cur == '[' || cur =='{')
+        {
+            if(idx == limit)
+            {
+                return false;
+            }
+            else
+            {
+                stack[idx++] = cur;
+            }
+        }
+        else
+        {
+            if(idx == 0)
+            {
+                return false;
+            }
+            peek = stack[idx -1];
+            if( (peek == '(' && cur == ')')
+                || (peek == '[' && cur == ']')
+                || (peek == '{' && cur == '}') )
+            {
+                idx--;
+            }
+        }
+    }
+    return idx == 0;
+}
+
+bool  isValidA(char* s) {  
     char *a;  
     int top; //top of stack  
       
@@ -33,8 +83,10 @@ bool  isValid(char* s) {
 }  
 int main(int argc, char const *argv[])
 {
-	char *str = "(1123, rsf[9[0]8}]sfds(44))";
-	bool result = isValid(str);
+	char *str = "(1123, r{sf[9[0]8]}sfds(44))";
+	bool result = isValidA(str);
 	printf("result == %d\n", result);
+    result = isValidB(str);
+    printf("result == %d\n", result);
 	return 0;
 }
